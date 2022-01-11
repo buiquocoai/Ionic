a822c458e6533d863cd5693338bf89cfaa0a237a
Quoc Oai <quocoai5285na@gmail.com>
2022-01-11 15:58:29 +0700

    first update code

---
+++ dashboard-page/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dashboard Page',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
