1bdaf4437f5822c92935c7fc7d48408a45884acf
BuiQuocOai <66450621+buiquocoai@users.noreply.github.com>
2022-01-17 16:15:46 +0700

    update code

--- ionic-bootstrap-tabs-app/src/app/home/home.page.ts
+++ ionic-bootstrap-tabs-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  subJects;
+  constructor(private router: Router) {}
   
-  constructor() {}
-  
+  ngOnInit(){
+    this.subJects =[
+      {
+        img: 'Assets/img/English',
+        name: 'English'
+      },
+      {
+        img: 'Assets/img/Statistics.png',
+        name: 'Statistics'
+      },
+      {
+        img: 'Assets/img/Mathamatics.png',
+        name: 'Mathamatics'
+      },
+      {
+        img: 'Assets/img/Physics.png',
+        name: 'Physics'
+      },
+      {
+        img: 'Assets/img/Social Sience.png',
+        name: 'Social Sience'
+      },
+      {
+        img: 'Assets/img/Social Sience1.png',
+        name: 'Social Sience1'
+      },
+
+    ]
+  }
+   
+  onClick(){
+    
+  }
 }
